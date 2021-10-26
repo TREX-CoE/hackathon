9954709c7f2728824251fff2e831881574552111
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-10-26 15:42:43 +0200

    adapt to having several org-mode files and htmlize.el

--- create.sh
+++ create.sh
@@ -1,14 +1,40 @@
 #!/bin/bash                                                                                                                   
-INPUT=$1
+readonly ORG_FILES="Autotools.org autoconf.org automake.org"
+readonly HTMLIZE=./docs/htmlize.el
 
-if [[ -f ../docs/htmlize.el ]]
+
+# Download the htmlize Emacs plugin if not present
+url="https://github.com/hniksic/emacs-htmlize"
+repo="emacs-htmlize"
+
+if [[ ! -f "${HTMLIZE}" ]]
 then
-    emacs --batch --load ../docs/htmlize.el --load ../docs/config.el $INPUT -f org-html-export-to-html
-else
-    emacs --batch --load ../docs/config.el $INPUT -f org-html-export-to-html
+    git clone ${url} \
+        && cp ${repo}/htmlize.el ${HTMLIZE} \
+        && rm -rf ${repo}
 fi
 
-mv ../Autotools.html index.html
+# Assert htmlize is installed
+[[ -f ${HTMLIZE} ]] \
+    || exit 1
 
 
+# Convert org to HTML
+if [[ -f ./docs/htmlize.el ]]
+then
+    for org in ${ORG_FILES}
+    do
+	echo "HTMLIZE: $org"
+    	emacs --batch --load ./docs/htmlize.el --load ./docs/config.el $org -f org-html-export-to-html
+    done
+else
+    for org in ${ORG_FILES}
+    do
+	echo "CONVERT: $org"
+    	emacs --batch --load ./docs/config.el $org -f org-html-export-to-html
+    done
+fi
+
+mv Autotools.html docs/index.html
+mv autoconf.html automake.html docs/
 
